58e3639d448fc08c943b9c042a6534ff5443a39e
Sean <siang720@gmail.com>
2019-11-28 20:24:43 +0800

    Add userController and signup page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const db = require("./models");
 const helpers = require("./_helpers");
 const exphbs = require("express-handlebars");
 const port = process.env.PORT || 3000;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const tweetController = require("../controllers/tweetController");
+const userController = require("../controllers/userController");
 module.exports = app => {
   //如果使用者訪問首頁，就導向 /tweets 的頁面
   app.get("/", (req, res) => res.redirect("/tweets"));
 
   //在 /tweets 底下則交給 restController.getTweets 來處理
   app.get("/tweets", tweetController.getTweets);
+
+  // signup
+  app.get("/signup", userController.signUpPage);
+  app.post("/signup", userController.signUp);
 };
