115355d3544a6f3c5e50d58efb26ada46ef1a3e2
Sean <siang720@gmail.com>
2019-11-29 00:38:10 +0800

    Add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
 const tweetController = require("../controllers/tweetController");
 const userController = require("../controllers/userController");
+const adminController = require("../controllers/adminController");
 module.exports = (app, passport) => {
+  // 驗證使用者權限
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/signin");
+  };
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.role === "admin") {
+        return next();
+      }
+      return res.redirect("/");
+    }
+    res.redirect("/signin");
+  };
+
   //如果使用者訪問首頁，就導向 /tweets 的頁面
-  app.get("/", (req, res) => res.redirect("/tweets"));
+  app.get("/", authenticated, (req, res) => res.redirect("/tweets"));
 
   //在 /tweets 底下則交給 restController.getTweets 來處理
-  app.get("/tweets", tweetController.getTweets);
+  app.get("/tweets", authenticated, tweetController.getTweets);
 
   // signup
   app.get("/signup", userController.signUpPage);
@@ -22,4 +40,10 @@ module.exports = (app, passport) => {
     userController.signIn
   );
   app.get("/logout", userController.logout);
+
+  // adminController
+  // app.get("/admin", authenticatedAdmin, (req, res) =>
+  //   res.redirect("/admin/tweets")
+  // );
+  // app.get("/admin/tweets", authenticatedAdmin, adminController.getTweets);
 };
