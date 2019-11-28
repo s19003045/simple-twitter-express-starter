e13a468a8ead51f02599237f58a9757f1e2a3b24
Ariel Huang <ariel92911@gmail.com>
2019-11-28 13:26:44 +0800

    Update admin tweet index page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,11 @@
+const db = require('../models')
+const Tweet = db.Tweet
+
 const adminController = {
   getTweets: (req, res) => {
-    return res.render('admin/tweets')
+    return Tweet.findAll().then(tweets => {
+      return res.render('admin/tweets', { tweets })
+    })
   }
 }
 
--- models/tweet.js
+++ models/tweet.js
@@ -1,10 +1,9 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
-  }, {
     description: DataTypes.TEXT,
     UserId: DataTypes.INTEGER
-  });
+  }, {});
   Tweet.associate = function (models) {
     Tweet.belongsTo(models.User)
     Tweet.hasMany(models.Reply)
