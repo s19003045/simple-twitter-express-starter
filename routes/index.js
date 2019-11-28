74cf3b4b06b461f825f81a256ea98d304be9edbd
Sean <siang720@gmail.com>
2019-11-29 00:04:35 +0800

    Add feature: signInPage, signIn and logout

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const tweetController = require("../controllers/tweetController");
 const userController = require("../controllers/userController");
-module.exports = app => {
+module.exports = (app, passport) => {
   //如果使用者訪問首頁，就導向 /tweets 的頁面
   app.get("/", (req, res) => res.redirect("/tweets"));
 
@@ -10,4 +10,16 @@ module.exports = app => {
   // signup
   app.get("/signup", userController.signUpPage);
   app.post("/signup", userController.signUp);
+
+  // signin
+  app.get("/signin", userController.signInPage);
+  app.post(
+    "/signin",
+    passport.authenticate("local", {
+      failureRedirect: "/signin",
+      failureFlash: true
+    }),
+    userController.signIn
+  );
+  app.get("/logout", userController.logout);
 };
