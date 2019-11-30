6794845f625cfc4fdfb8b84c45c60b0a7afb5179
Sean <siang720@gmail.com>
2019-11-30 16:15:01 +0800

    Add views and controllers for /tweets

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -1,6 +1,33 @@
+const db = require("../models");
+const Tweet = db.Tweet;
+const User = db.User;
+const Reply = db.Reply;
+const Like = db.Like;
+const Sequelize = require("sequelize");
+
 const tweetController = {
   getTweets: (req, res) => {
-    return res.render("tweets");
+    User.findAll({
+      include: [{ model: User, as: "Followers" }]
+    }).then(users => {
+      users.sort((a, b) =>
+        a.Followers.length > b.Followers.length
+          ? -1
+          : b.Followers.length > a.Followers.length
+          ? 1
+          : 0
+      );
+      let top_ten_users = users.slice(0, 10);
+      Tweet.findAndCountAll({
+        include: [User, Reply, Like],
+        order: [["updatedAt", "DESC"]]
+      }).then(result => {
+        res.render("tweets", {
+          tweets: result.rows,
+          top_ten_users
+        });
+      });
+    });
   }
 };
 
