6147ac12f92213f00f97a9a33de1ac9129062279
Ariel Huang <ariel92911@gmail.com>
2019-12-04 19:33:49 +0800

    Get likeArray

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -28,19 +28,41 @@ const adminController = {
   getUsers: (req, res) => {
     return User.findAll({
       include: [
-        Tweet,
-        { model: User, as: 'Followers' },
-        { model: User, as: 'Followings' },
+        {
+          model: Tweet,
+          attributes: ['id'],
+          include: [{
+            model: Like,
+            attributes: ['id']
+          }]
+        },
+        {
+          model: User,
+          as: 'Followers',
+          attributes: ['id']
+        },
+        {
+          model: User,
+          as: 'Followings',
+          attributes: ['id']
+        },
       ]
     }).then(users => {
+
+
       users = users.map(user => ({
         ...user.dataValues,
         // 計算推文篇數
         TweetCount: user.Tweets.length,
+        LikeArray: user.Tweets.map(function (obj) {
+          return obj.Likes.length
+        }),
       }))
+
       // 依推文篇數排序清單
       users = users.sort((a, b) => b.TweetCount - a.TweetCount)
-      console.log(users[8].Tweets)
+      console.log(users[8])
+
       return res.render('admin/users', { users })
     })
   }
