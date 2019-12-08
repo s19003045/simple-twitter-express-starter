d33a883f34bf8d28d9aaf8a34738ebcb80bac1e8
Gary Wu <s19003045@gmail.com>
2019-12-08 22:10:13 +0800

    modify seed file for user

--- seeders/20191130122958-Add-seed-user.js
+++ seeders/20191130122958-Add-seed-user.js
@@ -4,6 +4,44 @@ const bcrypt = require("bcrypt-nodejs");
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
+    // 建立 root(role:admin), user1(role:user), user2(role:user) 三個 user 種子
+    queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          name: "root",
+          email: "root@example.com",
+          password: bcrypt.hashSync("12345678", bcrypt.genSaltSync(10), null),
+          avatar: faker.image.imageUrl(),
+          introduction: faker.lorem.text().substring(0, 200),
+          role: "admin",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "user1",
+          email: "user1@example.com",
+          password: bcrypt.hashSync("12345678", bcrypt.genSaltSync(10), null),
+          avatar: faker.image.imageUrl(),
+          introduction: faker.lorem.text().substring(0, 200),
+          role: "user",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "user2",
+          email: "user2@example.com",
+          password: bcrypt.hashSync("12345678", bcrypt.genSaltSync(10), null),
+          avatar: faker.image.imageUrl(),
+          introduction: faker.lorem.text().substring(0, 200),
+          role: "user",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
+
     // 新增5個user資料
     return queryInterface.bulkInsert(
       "Users",
